277b68d3d42b9abd91b91e4852794b070bd4dac9
Sajal Basak <sajal.basak007@gmail.com>
2024-04-08 14:37:53 +0600

    design updated to legacy

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-const hamBurger = document.querySelector(".toggle-btn");
-
-hamBurger.addEventListener("click", function () {
-  document.querySelector("#sidebar").classList.toggle("expand");
-});
+function displayBigMenu(menuState) {
+  if (menuState) {
+      $('.side-menu').css({
+          width: '14.063rem'
+      });
+      $('.side-menu .side-bar a.menubutton').css({
+          width: '13.438rem',
+          'text-align': 'center'
+      });
+      $('.side-menu .side-bar a.menubutton').attr('data-toggle', "");
+      $('.side-menu .side-bar a.menubutton').attr('title', "");
+      $('.closeMenuButton').css({
+          display: 'block'
+      });
+      $('.showMenuButton').css({
+          display: 'none'
+      });
+  } else {
+      $('.side-menu').css({
+          width: '3.75rem'
+      });
+      $('.side-menu .side-bar a.menubutton').css({
+          width: '3rem',
+          'text-align': 'center'
+      });
+      $('.side-menu .side-bar a.menubutton').attr('data-toggle', "tooltip");
+      $('.closeMenuButton').css({
+          display: 'none'
+      });
+      $('.showMenuButton').css({
+          display: 'block'
+      });
+  }
+}
